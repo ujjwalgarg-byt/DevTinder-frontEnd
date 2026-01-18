7214b322fff66d4138da43864e5ee528263c953f
ujjwalgarg-byt <ujjwalgargofficial01@gmail.com>
2026-01-18 22:08:28 +0530

    built and add user card

---
+++ src/Component/UserCard.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const UserCard = ({user}) => {
+    console.log(user);
+    
+    const {firstName,lastName,photoUrl,about} = user;
+  return (
+    
+      <div className="card bg-base-300 w-96  shadow-sm">
+        <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            alt="Shoes"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">
+            {firstName+" "+lastName}
+          </h2>
+          <p>
+            {about}
+          </p>
+          <div className="card-actions flex justify-center my-4">
+            <div className="btn btn-primary">Ignored</div>
+            <div className="btn btn-secondary">Interested</div>
+          </div>
+        </div>
+      </div>
+    
+  );
+};
+
+export default UserCard;
